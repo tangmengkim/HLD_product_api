1a7d7409b4b37ae59a39408da6b4abf67dc24f61
MengKim Tang <88500719+tangmengkim@users.noreply.github.com>
2024-11-24 12:02:03 +0700

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const sequelize = new Sequelize(
   process.env.MSSQL_PASSWORD,
   {
     host: process.env.MSSQL_HOST,
-    dialect: PostgresDialect,
+    dialect: 'postgres',
     port: 5432,
     ssl: true,
     clientMinMessages: 'notice'
